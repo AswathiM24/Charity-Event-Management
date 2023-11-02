923d37c09241428cd3ef3038102d721cb3836063
Aswathi M <aswathim2402@gmail.com>
2023-11-02 23:59:20 +0530

    Connection created.

--- CEM/user/models.py
+++ CEM/user/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class User(models.Model):
     name=models.CharField(max_length=50)
-    email=models.EmailField()
+    email=models.EmailField(unique=True)
     phone=models.CharField(max_length=10)
     password=models.CharField(max_length=20)
     
--- CEM/user/views.py
+++ CEM/user/views.py
@@ -1,9 +1,25 @@
 from django.shortcuts import render
+from .models import User
 
 
 # Create your views here.
 def login(request):
     return render(request,'user/login.html')
 
+
+
 def signup(request):
+    res={}
+    if request.method=='POST':
+        username=request.POST['userName']
+        email=request.POST['email']
+        phone=request.POST['phone']
+        password=request.POST['password']
+        print(request.POST)
+        obj=User.objects.create(name=username,email=email,phone=phone,password=password)
+        obj.save()
+        print("User created successfully")
+        res['status']="Account created successfully."
+        return render(request,'user/signup.html',res)
     return render(request,'user/signup.html')
+    
