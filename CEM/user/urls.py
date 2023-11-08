6e8a8925c1a51a38c46429329c868cfb75c3eed9
Aswathi M <aswathim2402@gmail.com>
2023-11-08 23:52:20 +0530

    Signup in dashboard

--- CEM/user/urls.py
+++ CEM/user/urls.py
@@ -5,7 +5,9 @@ from .import views
 urlpatterns = [
     path('login',views.login,name='user_login'),
     path('signup',views.signup,name='user_signup'),
-    path('',views.dashboard,name='user_dashboard')
+    path('',views.dashboard,name='user_dashboard'),
+   
+    
    
     
 
