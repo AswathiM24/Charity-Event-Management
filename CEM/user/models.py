5472b6993053fbc2ba5c69ed5ea15f1d32257552
Aswathi M <aswathim2402@gmail.com>
2023-11-15 00:39:57 +0530

    Organization login table added.

--- CEM/user/models.py
+++ CEM/user/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.hashers import make_password
 
 # Create your models here.
 class User(models.Model):
@@ -13,6 +14,15 @@ class Organization(models.Model):
     name=models.CharField(max_length=50)
     email=models.EmailField(unique=True)
     phone=models.CharField(max_length=10)
-    password=models.CharField(max_length=20)
     def __str__(self):
         return self.name
+
+
+class Login(models.Model):
+    email=models.EmailField(unique=True)
+    password = models.CharField(max_length=255)
+    obj=models.OneToOneField(Organization,on_delete=models.CASCADE,related_name='org')
+
+    def save(self, *args, **kwargs):
+        self.password = make_password(self.password)
+        super().save(*args, **kwargs)
