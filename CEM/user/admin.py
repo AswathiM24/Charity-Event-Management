012d3d62e4236abce859e28bf9e91d81207d8251
Ajith Anand <ajithanand303@gmail.com>
2023-12-06 17:03:25 +0530

    Events

--- CEM/user/admin.py
+++ CEM/user/admin.py
@@ -1,6 +1,19 @@
 from django.contrib import admin
-from .models import User,Organization,Login
+from .models import User, Organization, Login, Events
+
 # Register your models here.
-admin.site.register(User)
-admin.site.register(Login)
-admin.site.register(Organization)
+# admin.site.register(User)
+# admin.site.register(Login)
+# admin.site.register(Organization)
+# admin.site.register(Events)
+
+from django.apps import apps
+
+
+models = apps.get_models()
+
+for model in models:
+    try:
+        admin.site.register(model)
+    except admin.sites.AlreadyRegistered:
+        pass
--- CEM/user/models.py
+++ CEM/user/models.py
@@ -1,31 +1,53 @@
 from django.db import models
 from django.contrib.auth.hashers import make_password
 
+
 # Create your models here.
 class User(models.Model):
-    name=models.CharField(max_length=50)
-    email=models.EmailField(unique=True)
-    phone=models.CharField(max_length=10)
-    password=models.CharField(max_length=20)
+    name = models.CharField(max_length=50)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=10)
+    password = models.CharField(max_length=20)
+
     def __str__(self):
         return self.name
-    
+
+
 class Organization(models.Model):
-    name=models.CharField(max_length=50)
-    email=models.EmailField(unique=True)
-    phone=models.CharField(max_length=10)
+    name = models.CharField(max_length=50)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=10)
     address = models.CharField(max_length=255)
     status = models.BooleanField(default=False)
     fund_raised = models.IntegerField(default=0)
+
     def __str__(self):
         return self.name
 
 
 class Login(models.Model):
-    email=models.EmailField(unique=True)
+    email = models.EmailField(unique=True)
     password = models.CharField(max_length=255)
-    obj=models.OneToOneField(Organization,on_delete=models.CASCADE,related_name='org')
+    obj = models.OneToOneField(Organization, on_delete=models.CASCADE, related_name='org')
 
     def save(self, *args, **kwargs):
         self.password = make_password(self.password)
         super().save(*args, **kwargs)
+
+
+class Events(models.Model):
+    Name = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    location = models.TextField()
+    org_name = models.CharField(max_length=255)
+    org_email = models.EmailField()
+    org_phone = models.IntegerField()
+    is_booking = models.BooleanField(default=False)
+    ticket_price = models.IntegerField()
+    is_active = models.BooleanField(default=True)
+    date = models.DateField(null=True, blank=True)
+    organization = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.Name
+
